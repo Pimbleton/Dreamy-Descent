3cd89931bab3d722649d6153a21c6db1558678ce
Hunter H. <hunter-harris1@hotmail.com>
2026-02-13 11:16:46 -0500

    Implemented proper collision + movement (for now)

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -1,33 +1,22 @@
-using NUnit.Framework;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerActions : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
+    public float moveSpeed = 5f;
 
-    void Update() {
-        checkMovement();
+    void FixedUpdate() {
+        movement();
     }
 
-    void checkMovement() {
-        //TODO - Integrate momentum/acceleration and implement collision detection
-        if (Keyboard.current.wKey.IsPressed() || Keyboard.current.sKey.IsPressed() || Keyboard.current.aKey.IsPressed() || Keyboard.current.dKey.IsPressed()) {
-            int north = Keyboard.current.wKey.IsPressed() ? 1 : 0;
-            int south = Keyboard.current.sKey.IsPressed() ? 1 : 0;
-            int east = Keyboard.current.dKey.IsPressed() ? 1 : 0;
-            int west = Keyboard.current.aKey.IsPressed() ? 1 : 0;
+    void movement() {
+        Vector2 movementVector = new Vector2(
+            Keyboard.current.dKey.IsPressed() ? 1 : 0 - (Keyboard.current.aKey.IsPressed() ? 1 : 0), 
+            Keyboard.current.wKey.IsPressed() ? 1 : 0 - (Keyboard.current.sKey.IsPressed() ? 1 : 0)).normalized;
 
-            //if (!OnCollisionEnter(myRigidbody)) {
-                myRigidbody.linearVelocity = Vector2.up * 10 * north + Vector2.down * 10 * south + Vector2.left * 10 * west + Vector2.right * 10 * east;
-            //}
-            //else {
-            //    myRigidbody.linearVelocity = Vector2.zero;
-           // }
-        }
-        else {
-            myRigidbody.linearVelocity = Vector2.zero;
-        }
+        Vector2 targetPosition = myRigidbody.position + movementVector * moveSpeed * Time.fixedDeltaTime;
+
+        myRigidbody.MovePosition(targetPosition);
     }
 }
