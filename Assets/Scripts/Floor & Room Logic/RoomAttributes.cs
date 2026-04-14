6590deb8095d4f931f1251afe34ef6ff3ee1d9ba
Hunter H. <hunter-harris1@hotmail.com>
2026-04-14 17:46:45 -0400

    Successfully implemented clear condition for rooms
    If enemies exist in a room, the room is marked as not cleared until all enemies within the room are eradicated, making the player unable to leave the room until that condition is met.

--- Assets/Scripts/Floor & Room Logic/RoomAttributes.cs
+++ Assets/Scripts/Floor & Room Logic/RoomAttributes.cs
@@ -1,35 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D.Animation;
 
 public class RoomAttributes : MonoBehaviour {
     [Header("Doors")]
-    public GameObject doorNorth;
-    public GameObject doorSouth;
-    public GameObject doorEast;
-    public GameObject doorWest;
+    public GameObject doorNorth, doorEast, doorSouth, doorWest;
 
     [Header("Neighbors")]
-    public GameObject northRoom;
-    public GameObject southRoom;
-    public GameObject eastRoom;
-    public GameObject westRoom;
+    public GameObject northRoom, eastRoom, southRoom, westRoom;
 
     [Header("Colliders")]
-    private Transform playerCollidersContainer;
-    private Transform projectileCollidersContainer;
+    private Transform playerCollidersContainer, projectileCollidersContainer;
 
     [Header("Misc")]
+    public bool cleared;
     private SpriteRenderer cachedBounds;
+    private SpriteResolver resolverN, resolverE, resolverS, resolverW;
     public string roomType;
 
     public Vector2Int gridPos;
 
     void Awake() {
-        doorNorth = transform.Find("N_Door").gameObject;
-        doorSouth = transform.Find("S_Door").gameObject;
-        doorEast = transform.Find("E_Door").gameObject;
-        doorWest = transform.Find("W_Door").gameObject;
+        doorNorth = transform.Find("N_Door")?.gameObject;
+        doorSouth = transform.Find("S_Door")?.gameObject;
+        doorEast = transform.Find("E_Door")?.gameObject;
+        doorWest = transform.Find("W_Door")?.gameObject;
+
+        if (doorNorth) {
+            resolverN = doorNorth.GetComponent<SpriteResolver>();
+        }
         
+        if (doorSouth) {
+            resolverS = doorSouth.GetComponent<SpriteResolver>();
+        }
+        
+        if (doorEast) {
+            resolverE = doorEast.GetComponent<SpriteResolver>();
+        }
+        
+        if (doorWest) {
+            resolverW = doorWest.GetComponent<SpriteResolver>();
+        }
+
         playerCollidersContainer = transform.Find("PlayerCollider");  
         projectileCollidersContainer = transform.Find("ProjectileCollider");
 
@@ -38,10 +50,18 @@ public class RoomAttributes : MonoBehaviour {
             cachedBounds = boundsTransform.GetComponent<SpriteRenderer>();
         }
     }
-    
+
     void Start() {
         if (gameObject.activeInHierarchy) {
                 InitializeCamera();
+        }
+
+        CheckIfCleared();
+    }
+
+    void Update() {
+        if(!cleared && gameObject.activeInHierarchy) {
+            CheckIfCleared();
         }
     }
 
@@ -63,6 +83,52 @@ public class RoomAttributes : MonoBehaviour {
         doorWest.SetActive(hasWest);
 
         ApplyCollider(hasNorth, hasSouth, hasEast, hasWest);
+    }
+
+    private void CheckIfCleared() {
+        bool enemyFound = false;
+
+        // Loop through the children of this room only
+        foreach (Transform child in transform) {
+            if (child.CompareTag("Enemy")) {
+                enemyFound = true;
+                break; // We found one, no need to keep looking
+            }
+        }
+
+        if (!enemyFound) {
+            cleared = true;
+            UpdateDoorStates();
+        } else {
+            cleared = false;
+            UpdateDoorStates();
+        }
+    }
+
+    public void UpdateDoorStates() {
+        string label = cleared ? "Open" : "Closed";
+
+        SetDoorState(doorNorth, resolverN, label);
+        SetDoorState(doorSouth, resolverS, label);
+        SetDoorState(doorEast, resolverE, label);
+        SetDoorState(doorWest, resolverW, label);
+    }
+
+    private void SetDoorState(GameObject doorObj, SpriteResolver resolver, string label) {
+        if (doorObj == null) {
+            return;
+        }
+
+        // Swap Sprite
+        if (resolver != null) {
+            resolver.SetCategoryAndLabel("Doors", label);
+        }
+
+        // Toggles box collider
+        BoxCollider2D col = doorObj.GetComponent<BoxCollider2D>();
+        if (cleared) {
+            Destroy(col);
+        }
     }
 
     void ApplyCollider(bool n, bool s, bool e, bool w) {
